d04edfb77b2e56f52ad01a6f29706bc962792de0
Bryce <brycemwilson@gmail.com>
2016-01-28 13:56:37 -0600

    Add GUI to interface

---
+++ gui.h
@@ -0,0 +1,15 @@
+#ifndef GUI_H
+#define GUI_H
+
+#include <stdint.h>
+
+/* enumerations for the virtual GUI display. */
+enum{
+  TV_GUI_W = 320,
+  TV_GUI_H = 240
+};
+
+void tv_GuiRect(unsigned,unsigned,unsigned,unsigned,uint32_t);
+void tv_GuiText(unsigned,unsigned,const char*);
+
+#endif
